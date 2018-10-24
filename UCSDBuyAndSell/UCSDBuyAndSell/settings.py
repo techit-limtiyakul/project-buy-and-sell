0cfb79aea299c7c43ca8604cc5ea200d21775fe9
Peter Greer-Berezovsky <pbgreerb@gmail.com>
2018-10-24 15:41:57 -0700

    Update UCSDBuyAndSell/UCSDBuyAndSell/settings.py
    Adding local host to annowed hosts

--- UCSDBuyAndSell/UCSDBuyAndSell/settings.py
+++ UCSDBuyAndSell/UCSDBuyAndSell/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r#)pdysojr!xc=&%gncz7bc8-n78zrrkvpxaq@qu%oxp-r=$gy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ucsdbuyandsell.pythonanywhere.com"]
+ALLOWED_HOSTS = ["ucsdbuyandsell.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
