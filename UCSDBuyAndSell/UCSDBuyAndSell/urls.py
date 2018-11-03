d90e2065ade3c1fee12e2204aafd0d489a3a3af2
Peter Greer-Berezovsky <pbgreerb@gmail.com>
2018-11-03 01:54:53 -0700

    Webapp setup
    Created a webapp in our django project and fixed some annoying settings that have been been troublesome in past projects.
    
    If you want to add a new webpage you can do so by putting the html into the Templates\UCSDMarket folder, adding a render function into UCSDMarket\views.py, and calling that render function from UCSDBuyAndSell\urls.py

--- UCSDBuyAndSell/UCSDBuyAndSell/urls.py
+++ UCSDBuyAndSell/UCSDBuyAndSell/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
+from UCSDMarket import views as UCSDMarket
 
 urlpatterns = [
+	url(r'^$', UCSDMarket.Home),
     path('admin/', admin.site.urls),
 ]
---
+++ UCSDBuyAndSell/UCSDMarket/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UcsdmarketConfig(AppConfig):
+    name = 'UCSDMarket'
