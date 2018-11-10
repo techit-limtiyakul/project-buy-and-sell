24fada133eedf73b167731db019038b27e2265ff
Ning <ning@Ningss-MacBook-Pro.local>
2018-11-09 17:09:18 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	UCSDBuyAndSell/UCSDMarket/forms.py
    #	UCSDBuyAndSell/UCSDMarket/views.py
    #	UCSDBuyAndSell/db.sqlite3

--- UCSDBuyAndSell/UCSDMarket/forms.py
+++ UCSDBuyAndSell/UCSDMarket/forms.py
@@ -1,6 +1,19 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class LoginForm(AuthenticationForm):
 	username=forms.CharField(label="",widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'UCSD Email'}))
 	password=forms.CharField(label="",widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+
+class SignupForm(UserCreationForm):
+	username=forms.CharField(label="Email", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'UCSD Email'}))
+	password1=forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+	password2=forms.CharField(label="Retype Password", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Retype Password'}))
+
+class CreateListingForm(forms.Form):
+	renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+
+
+class ImageUploadForm(forms.Form):
+	image = forms.ImageField()#forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
--- UCSDBuyAndSell/UCSDMarket/views.py
+++ UCSDBuyAndSell/UCSDMarket/views.py
@@ -1,58 +1,122 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login, authenticate
+from UCSDMarket.forms import SignupForm, ImageUploadForm
+from UCSDMarket.models import Picture
 
 # Create your views here.
 def Home(request):
     context = { } #
     return render(request, "UCSDMarket/home.html", context)
 
+def Signup(request):
+    form = SignupForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        compare_password = form.cleaned_data.get('password2')
+        if raw_password != compare_password:
+            form.add_error('compare_password', "Password does not match")
+            return redirect('signup')
+        user = authenticate(username=username, password=raw_password)
+        login(request, user)
+        return redirect('Home')
+    return render(request, 'UCSDMarket/signup.html', {'form': form})
+
+def Signoff(request):
+    logout(request)
+    response = redirect('/market/login')
+    return response
+
 def Listing(request):
-	
+	all_entries = Picture.objects.all()
+
+	all_pictures = []
+
+	imgCount = 0
+	for entry in all_entries:
+		imgCount = imgCount + 1
+		all_pictures.append({
+							"Image": entry,
+							"Number": imgCount
+							})
+
 	context = {
 		"Title" : "IKEA full size bed",
 		"Seller" : "John Doe",
 		"Price" : 50,
 		"CanDeliver" : True,
 		"Condition" : "Used",
-		"Description" : "Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129",
-		"ContactInformation" : "858 - 888 - 8888"
-	} #
+		"Description" : "Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129",
+		"ContactInformation" : "858 - 888 - 8888",
+		"Pictures": all_pictures
+	}
+
 	return render(request, "UCSDMarket/listing.html", context)
 
 def MyListings(request):
-	
-	Listings = []
-	
-	Listings.append({
-		"Title" : "IKEA full size bed",
-		"Seller" : "John Doe",
-		"Price" : 50,
-		"CanDeliver" : True,
-		"Condition" : "Used",
-		"Description" : "Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129",
-		"ContactInformation" : "858 - 888 - 8888"
-	})
-	
-	Listings.append({
-		"Title" : "King Sized Bed",
-		"Seller" : "Reggie Smiles",
-		"Price" : 150,
-		"CanDeliver" : False,
-		"Condition" : "New",
-		"Description" : "Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129",
-		"ContactInformation" : "858 - 888 - 8888"
-	})
-	
-	Listings.append({
-		"Title" : "Comfy Cot",
-		"Seller" : "John Doe",
-		"Price" : 30,
-		"CanDeliver" : True,
-		"Condition" : "Used",
-		"Description" : "Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129",
-		"ContactInformation" : "858 - 888 - 8888"
-	})
+	if request.user.is_authenticated:
+		# Get listings from user
+		Listings = []
+
+		all_entries = Picture.objects.all()
+		thumbImg = all_entries[:1].get()
+
+		Listings.append({
+			"Title" : "IKEA full size bed",
+			"Seller" : "John Doe",
+			"Price" : 50,
+			"CanDeliver" : True,
+			"Condition" : "Used",
+			"Description" : "Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129",
+			"ContactInformation" : "858 - 888 - 8888",
+			"Thumbnail": thumbImg
+		})
+
+		Listings.append({
+			"Title" : "King Sized Bed",
+			"Seller" : "Reggie Smiles",
+			"Price" : 150,
+			"CanDeliver" : False,
+			"Condition" : "New",
+			"Description" : "Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129",
+			"ContactInformation" : "858 - 888 - 8888",
+			"Thumbnail": thumbImg
+		})
+
+		Listings.append({
+			"Title" : "Comfy Cot",
+			"Seller" : "John Doe",
+			"Price" : 30,
+			"CanDeliver" : True,
+			"Condition" : "Used",
+			"Description" : "Need this gone by Oct 31. Great condition. Can deliver for some extra fee. Original price was $129",
+			"ContactInformation" : "858 - 888 - 8888",
+			"Thumbnail": thumbImg
+		})
+
+		context = {
+			"Listings" : Listings,
+		} #
+		return render(request, "UCSDMarket/my_listings.html", context)
+	else:
+		return render(request, "UCSDMarket/home.html")
+
+def CreateListings(request):
 	
 	context = {
-		"Listings" : Listings,
+		"Title" : "Create my listing here!",
+		"Description" : "Please fill out the following form to post your item."
+
 	} #
-	return render(request, "UCSDMarket/my_listings.html", context)
+	return render(request, "UCSDMarket/create_listing.html", context)
+
+
+def SearchListings(request):
+	
+	context = {
+		"Title" : "Search and explore what you want!",
+		"Description" : "Please enter the information you would like to search."
+
+	} #
+	return render(request, "UCSDMarket/search_listing.html", context)
