f6fd9bd88400323433169db214383680829d6e63
Peter Tueller <ptueller7@gmail.com>
2018-11-10 20:30:34 -0800

    Added UCSD email check Currently it doesn't display an error (I think that requires additional front end work), it just redirects to the signin page

--- UCSDBuyAndSell/UCSDMarket/views.py
+++ UCSDBuyAndSell/UCSDMarket/views.py
@@ -12,13 +12,16 @@ def Home(request):
 def Signup(request):
     form = SignupForm(request.POST)
     if form.is_valid():
-        form.save()
         username = form.cleaned_data.get('username')
+        if not username.endswith('ucsd.edu'):
+            form.add_error('username', "Email doesn't end with ucsd.edu")
+            return redirect('Signup')
         raw_password = form.cleaned_data.get('password1')
         compare_password = form.cleaned_data.get('password2')
         if raw_password != compare_password:
             form.add_error('compare_password', "Password does not match")
-            return redirect('signup')
+            return redirect('Signup')
+        form.save()
         user = authenticate(username=username, password=raw_password)
         login(request, user)
         return redirect('Home')
@@ -30,98 +33,98 @@ def Signoff(request):
     return response
 
 def ListingPage(request):
-	if request.method=='GET':
-		listingID = request.GET.get('listing')
-		if not listingID:
-			return render(request, 'UCSDMarket/home.html')
-		else:
-			ThisListing = Listing.objects.filter(id=int(listingID))
-			if (len(ThisListing) == 1):
-				ThisListing = ThisListing[0]
+    if request.method=='GET':
+        listingID = request.GET.get('listing')
+        if not listingID:
+            return render(request, 'UCSDMarket/home.html')
+        else:
+            ThisListing = Listing.objects.filter(id=int(listingID))
+            if (len(ThisListing) == 1):
+                ThisListing = ThisListing[0]
 
-				images = Picture.objects.filter(listingKey=ThisListing)
-				imgCount = 0
-				all_pictures = []
-				for image in images:
-					imgCount = imgCount + 1
-					all_pictures.append({
-										"Image": image,
-										"Number": imgCount
-										})
-				
-				context = {
-					"id" : ThisListing.id,
-					"Title" : ThisListing.title,
-					"Seller" : ThisListing.user.username,
-					"Price" : ThisListing.price,
-					"CanDeliver" : ThisListing.canDeliver,
-					"Condition" : ThisListing.condition,
-					"Description" : ThisListing.description,
-					"ContactInformation" : ThisListing.contactInformation,
-					"Pictures": all_pictures
-				}
-				
-				return render(request, "UCSDMarket/listing.html", context)
-			else:
-				#Something has gone wrong!
-				return render(request, 'UCSDMarket/home.html')
+                images = Picture.objects.filter(listingKey=ThisListing)
+                imgCount = 0
+                all_pictures = []
+                for image in images:
+                    imgCount = imgCount + 1
+                    all_pictures.append({
+                                        "Image": image,
+                                        "Number": imgCount
+                                        })
+
+                context = {
+                    "id" : ThisListing.id,
+                    "Title" : ThisListing.title,
+                    "Seller" : ThisListing.user.username,
+                    "Price" : ThisListing.price,
+                    "CanDeliver" : ThisListing.canDeliver,
+                    "Condition" : ThisListing.condition,
+                    "Description" : ThisListing.description,
+                    "ContactInformation" : ThisListing.contactInformation,
+                    "Pictures": all_pictures
+                }
+
+                return render(request, "UCSDMarket/listing.html", context)
+            else:
+                #Something has gone wrong!
+                return render(request, 'UCSDMarket/home.html')
 
 def MyListings(request):
 
-	if request.user.is_authenticated:
-		# Get listings from user
-		MyListings = Listing.objects.filter(user=request.user)
-		Listings = []
-		
-		for post in MyListings:
-			all_images = Picture.objects.filter(listingKey=post)
-			if not all_images:
-				thumbImg = False
-			else:
-				thumbImg = all_images[0]
-			Listings.append({
-				"id" : post.id,
-				"Title" : post.title,
-				"Seller" : post.user.username,
-				"Price" : post.price,
-				"CanDeliver" : post.canDeliver,
-				"Condition" : post.condition,
-				"Description" : post.description,
-				"ContactInformation" : post.contactInformation,
-				"Thumbnail": thumbImg
-			})
+    if request.user.is_authenticated:
+        # Get listings from user
+        MyListings = Listing.objects.filter(user=request.user)
+        Listings = []
 
-		context = {
-			"Listings" : Listings,
-		}
-		return render(request, "UCSDMarket/my_listings.html", context)
-	else:
-		return render(request, "UCSDMarket/home.html")
+        for post in MyListings:
+            all_images = Picture.objects.filter(listingKey=post)
+            if not all_images:
+                thumbImg = False
+            else:
+                thumbImg = all_images[0]
+            Listings.append({
+                "id" : post.id,
+                "Title" : post.title,
+                "Seller" : post.user.username,
+                "Price" : post.price,
+                "CanDeliver" : post.canDeliver,
+                "Condition" : post.condition,
+                "Description" : post.description,
+                "ContactInformation" : post.contactInformation,
+                "Thumbnail": thumbImg
+            })
+
+        context = {
+            "Listings" : Listings,
+        }
+        return render(request, "UCSDMarket/my_listings.html", context)
+    else:
+        return render(request, "UCSDMarket/home.html")
 
 def CreateListings(request):
-	if request.user.is_authenticated:
-		if request.method == 'POST':
-			form = CreateListingForm(request.POST, request.FILES)
-			if form.is_valid():
-				
-				deliverable = False
-				if request.POST.get('canDeliver', False):
-					deliverable = True
-					
-				newListing = Listing(
-				user = request.user,
-				title = request.POST['title'],
-				price = request.POST['price'],
-				canDeliver = deliverable,
-				condition = request.POST['condition'],
-				description = request.POST['description'],
-				contactInformation = request.POST['contactInformation'])
-		
-				newListing.save()
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = CreateListingForm(request.POST, request.FILES)
+            if form.is_valid():
+
+                deliverable = False
+                if request.POST.get('canDeliver', False):
+                    deliverable = True
+
+                newListing = Listing(
+                user = request.user,
+                title = request.POST['title'],
+                price = request.POST['price'],
+                canDeliver = deliverable,
+                condition = request.POST['condition'],
+                description = request.POST['description'],
+                contactInformation = request.POST['contactInformation'])
+
+                newListing.save()
                 # save uploaded picture to the database along with the id of the listing
-				if request.POST.get('image', False):
-					newPic = Picture(listingKey = newListing, picture=request.FILES['image'])
-					newPic.save()
+                if request.POST.get('image', False):
+                    newPic = Picture(listingKey = newListing, picture=request.FILES['image'])
+                    newPic.save()
 #					Upload multiple images
 #					for i in range(len(request.FILES['image'])):
 #						m = Picture(listingKey = newListing, picture = request.FILES['image'][i])
@@ -130,26 +133,26 @@ def CreateListings(request):
 #						pass
 #						# form = CreateListingForm();
 #						# TODO give error message: form is not valid
-		else:
-			form = CreateListingForm()
-		context = {
-			"Title" : "Create my listing here!",
-			"Description" : "Please fill out the following form to post your item.",
-			"form" : form
-		} 
-		return render(request, "UCSDMarket/create_listing.html", context)
-	else:
-		# TODO give error message: user not authenticated
-		return render(request, "UCSDMarket/home.html")
+        else:
+            form = CreateListingForm()
+        context = {
+            "Title" : "Create my listing here!",
+            "Description" : "Please fill out the following form to post your item.",
+            "form" : form
+        }
+        return render(request, "UCSDMarket/create_listing.html", context)
+    else:
+        # TODO give error message: user not authenticated
+        return render(request, "UCSDMarket/home.html")
 
 
 def SearchListings(request):
-	
-	context = {
-		"Title" : "Search and explore what you want!",
-		"Description" : "Please enter the information you would like to search.", 
-		"Results" : "Here are the results."
 
-	} #
-	return render(request, "UCSDMarket/search_listing.html", context)
+    context = {
+        "Title" : "Search and explore what you want!",
+        "Description" : "Please enter the information you would like to search.",
+        "Results" : "Here are the results."
+
+    } #
+    return render(request, "UCSDMarket/search_listing.html", context)
 
