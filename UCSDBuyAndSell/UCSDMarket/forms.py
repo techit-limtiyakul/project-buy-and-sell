04fb4dbd113339aa32a3003dcd7bfa4711361c22
Unknown <zanarkand.th@gmail.com>
2018-11-10 16:55:37 -0800

    minor ui tweak

--- UCSDBuyAndSell/UCSDMarket/forms.py
+++ UCSDBuyAndSell/UCSDMarket/forms.py
@@ -7,9 +7,9 @@ class LoginForm(AuthenticationForm):
 	password=forms.CharField(label="",widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
 
 class SignupForm(UserCreationForm):
-	username=forms.CharField(label="Email", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'UCSD Email'}))
-	password1=forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
-	password2=forms.CharField(label="Retype Password", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Retype Password'}))
+	username=forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'UCSD Email'}))
+	password1=forms.CharField(label="", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+	password2=forms.CharField(label="", widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Retype Password'}))
 
 class CreateListingForm(forms.Form):
 	renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
